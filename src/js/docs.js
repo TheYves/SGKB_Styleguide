0a78eb5ab09fc0ac58f528492c1665aa8771df52
Yves Hohl <yh@conx.ch>
2017-05-15 16:08:26 +0200

    dynamic navbar

--- src/js/docs.js
+++ src/js/docs.js
@@ -3,7 +3,12 @@ $(function () {
 	initDocs();
 
 	function initDocs() {
-		// init highlight.js
+		initHighlightJs();
+		initSidebar();
+		initScrollspy();
+	}
+
+	function initHighlightJs() {
 		$('pre code').each(function (i, block) {
 			var $block = $(block);
 			var html = $block.html();
@@ -14,11 +19,37 @@ $(function () {
 			var fixed = hljs.fixMarkup(hljs.highlightAuto(html).value);
 			$block.html(fixed);
 		});
+	}
 
+	function initScrollspy() {
 		$('body').scrollspy({
 			target: '.bs-docs-sidebar',
 			offset: 40
 		});
+	}
+
+	function initSidebar() {
+		var $menu = $('<ul class="nav nav-stacked fixed"></ul>');
+
+		$('.group').each(function () {
+			var $group = $(this);
+			var id = $group.attr('id');
+			var title = $group.find('> h2,> h3,> h4,> h5,> h6').first().text();
+			var $menuItem = $('<li><a href="#' + id + '">' + title + '</a><ul class="nav nav-stacked"></ul></li>');
+			var $menuNav = $menuItem.find('.nav');
+
+			$group.find('.subgroup').each(function () {
+				var $subgroup = $(this);
+				var id = $subgroup.attr('id');
+				var title = $subgroup.find('> h2,> h3,> h4,> h5,> h6').first().text();
+				var $menuSubItem = $('<li><a href="#' + id + '">' + title + '</a></li>');
+				$menuNav.append($menuSubItem);
+			});
+
+			$menu.append($menuItem);
+		});
+
+		$('#sidebar').append($menu);
 	}
 
 });
