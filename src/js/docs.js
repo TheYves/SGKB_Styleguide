fa9dc7ac5e9cb8bdf6509d427a62807669ce0b8c
Thomas Veit <thomas.veit@tommyholiday.ch>
2017-09-27 11:10:01 +0200

    changed structure

--- src/js/docs.js
+++ src/js/docs.js
@@ -34,14 +34,14 @@ $(function () {
 		$('.group').each(function () {
 			var $group = $(this);
 			var id = $group.attr('id');
-			var title = $group.find('> h2,> h3,> h4,> h5,> h6').first().text();
+			var title = $group.find('> h1, > h2,> h3,> h4,> h5,> h6').first().text();
 			var $menuItem = $('<li><a href="#' + id + '">' + title + '</a><ul class="nav nav-stacked"></ul></li>');
 			var $menuNav = $menuItem.find('.nav');
 
 			$group.find('.subgroup').each(function () {
 				var $subgroup = $(this);
 				var id = $subgroup.attr('id');
-				var title = $subgroup.find('> h2,> h3,> h4,> h5,> h6').first().text();
+				var title = $subgroup.find('> h1, > h2,> h3,> h4,> h5,> h6').first().text();
 				var $menuSubItem = $('<li><a href="#' + id + '">' + title + '</a></li>');
 				$menuNav.append($menuSubItem);
 			});
@@ -53,4 +53,3 @@ $(function () {
 	}
 
 });
-
